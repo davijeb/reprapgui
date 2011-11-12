f93912cc01eeb5837a4cb27c0edbfca9f2b368bd
Jeremy Davies <jerdavies@gmail.com>
2011-11-12 19:03:38 +0000

    POM and the first test classes to check we can connect to the printer

---
+++ reprapgui/src/test/java/com/reprap/reprapgui/RepRapPrinterConnectTest.java
@@ -0,0 +1,12 @@
+package com.reprap.reprapgui;
+
+public class RepRapPrinterConnectTest {
+	
+	private final FFFPrinter printer = new RepRapPrinter();
+	
+	@T
+	public void connectToPrinterSuccessfully() {
+		
+	}
+
+}
