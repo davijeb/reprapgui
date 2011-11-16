1e6250c9bcd1d424d57e7443726ff5c04b9aaa68
Jeremy Davies <jerdavies@gmail.com>
2011-11-16 22:29:17 +0000

    deleted

--- reprapgui/src/main/java/com/reprap/reprapgui/printers/CartesianPrinter.java
+++
@@ -1,9 +0,0 @@
-package com.reprap.reprapgui.printers;
-
-/**
- * The CartesianPrinter interface defines the methods required
- * of the various printers we may have available.
- */
-public interface CartesianPrinter {
-	void connect();
-}
