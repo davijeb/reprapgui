5f5a503f44d22e7c90b94b09568d780760895435
Jeremy Davies <jerdavies@gmail.com>
2011-11-16 22:28:29 +0000

    Simplified this to just have a single button and label plus a message dialogue which pops up when the button is pressed.
    The printer is now autowired

--- reprapgui/src/main/java/com/reprap/reprapgui/gui/RepRapWindow.java
+++ reprapgui/src/main/java/com/reprap/reprapgui/gui/RepRapWindow.java
@@ -1,41 +1,75 @@
 package com.reprap.reprapgui.gui;
 
-import java.util.Observable;
-import java.util.Observer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-import com.reprap.reprapgui.Main;
+import net.miginfocom.swing.MigLayout;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.reprap.reprapgui.config.PrinterButton;
 import com.reprap.reprapgui.config.PrinterStatus;
+import com.reprap.reprapgui.printer.PrusaPrinter;
 
 /**
- * The RepRapWindow is the main User Interface for controlling
- * the printer, defining the parameters, starting builds etc.
+ * The RepRapWindow is the main User Interface for controlling the printer,
+ * defining the parameters, starting builds etc.
  */
-public class RepRapWindow extends JFrame implements Observer {
-	
+public class RepRapWindow extends JFrame {
+
 	private static final long serialVersionUID = -7059521570271326438L;
 
-	public static final String CONNECTION_LABEL_NAME = "ConnectionLabel";
+	public static final String MAIN_WINDOW_NAME  = "Main";
+	public static final String CONNECTION_LABEL_NAME  = "ConnectionLabel";
+	public static final String CONNECTION_BUTTON_NAME = "ConnectionButton";
 	
-	private final JLabel label = new JLabel(PrinterStatus.Disconnected.toString());
+	@Autowired
+	private PrusaPrinter printer;
+
+	private final JLabel label   = new JLabel(PrinterStatus.Disconnected.toString());
+	private final JButton button = new JButton(PrinterButton.Connect.toString());
 	
-	public RepRapWindow() {
-		super("RepRap GUI");
-		setName(Main.MAIN_WINDOW_NAME);
+	public RepRapWindow(final String name) {
+		super(name);
+		setName(name);
+		
 		label.setName(CONNECTION_LABEL_NAME);
-		add(label);
+		button.setName(CONNECTION_BUTTON_NAME);
+
+		final JPanel panel = new JPanel(new MigLayout());
+
+		panel.add(button);
+		panel.add(label);
+		add(panel);
+		
+		button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(final ActionEvent arg0) {
+				JOptionPane.showMessageDialog(getFrame(), printer);
+			}
+		});
+		
 		pack();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
+	
+	JFrame getFrame() {
+		return this;
+	}
 
 	/**
-	 * Update the printer status messages when observable changes are detected.
+	 * Entry point to application
+	 * 
+	 * @param args the runtime arg array
 	 */
-	@Override
-	public void update(final Observable observable, final Object observerValue) {
-		label.setText(observerValue.toString());
-	}
+	public static void main(final String[] args) {new ClassPathXmlApplicationContext("application-context.xml").getBean("MainApplication");}
 }
