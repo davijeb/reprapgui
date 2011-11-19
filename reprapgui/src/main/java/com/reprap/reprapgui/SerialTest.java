89c595597a53ea330bab60a1b1534b425b9f15ee
Jeremy Davies <jerdavies@gmail.com>
2011-11-19 21:27:43 +0000

    Will have to move this but its the raw connection code for the Arduino board. At present it has 0 test coverage

---
+++ reprapgui/src/main/java/com/reprap/reprapgui/SerialTest.java
@@ -0,0 +1,112 @@
+package com.reprap.reprapgui;
+
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+
+public class SerialTest implements SerialPortEventListener {
+	SerialPort serialPort;
+        /** The port we're normally going to use. */
+	private static final String PORT_NAMES[] = { 
+			"/dev/tty.usbserial-A900F8BW", // Mac OS X
+			"/dev/ttyUSB0", // Linux
+			"COM3", // Windows
+			};
+	/** Buffered input stream from the port */
+	private InputStream input;
+	/** The output stream to the port */
+	private OutputStream output;
+	/** Milliseconds to block while waiting for port open */
+	private static final int TIME_OUT = 2000;
+	/** Default bits per second for COM port. */
+	private static final int DATA_RATE = 9600;
+
+	public void initialize() {
+		CommPortIdentifier portId = null;
+		final Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+
+		// iterate through, looking for the port
+		while (portEnum.hasMoreElements()) {
+			final CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+			for (final String portName : PORT_NAMES) {
+				if (currPortId.getName().equals(portName)) {
+					portId = currPortId;
+					break;
+				}
+			}
+		}
+
+		if (portId == null) {
+			System.out.println("Could not find COM port.");
+			return;
+		}
+
+		try {
+			// open serial port, and use class name for the appName.
+			serialPort = (SerialPort) portId.open(this.getClass().getName(),
+					TIME_OUT);
+
+			// set port parameters
+			serialPort.setSerialPortParams(DATA_RATE,
+					SerialPort.DATABITS_8,
+					SerialPort.STOPBITS_1,
+					SerialPort.PARITY_NONE);
+
+			// open the streams
+			input = serialPort.getInputStream();
+			output = serialPort.getOutputStream();
+
+			// add event listeners
+			serialPort.addEventListener(this);
+			serialPort.notifyOnDataAvailable(true);
+			
+			serialPort.sendBreak(10);
+			
+		} catch (final Exception e) {
+			System.err.println(e.toString());
+		}
+	}
+
+	/**
+	 * This should be called when you stop using the port.
+	 * This will prevent port locking on platforms like Linux.
+	 */
+	public synchronized void close() {
+		if (serialPort != null) {
+			serialPort.removeEventListener();
+			serialPort.close();
+		}
+	}
+
+	/**
+	 * Handle an event on the serial port. Read the data and print it.
+	 */
+	@Override
+	public synchronized void serialEvent(final SerialPortEvent oEvent) {
+		if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+			try {
+				final int available = input.available();
+				final byte chunk[] = new byte[available];
+				input.read(chunk, 0, available);
+
+				// Displayed results are codepage dependent
+				System.out.print(new String(chunk));
+			} catch (final Exception e) {
+				System.err.println(e.toString());
+			}
+		}
+		// Ignore all the other eventTypes, but you should consider the other ones.
+	}
+
+	public static void main(final String[] args) throws Exception {
+		final SerialTest main = new SerialTest();
+		main.initialize();
+		System.out.println("Started");
+		
+	}
+}
