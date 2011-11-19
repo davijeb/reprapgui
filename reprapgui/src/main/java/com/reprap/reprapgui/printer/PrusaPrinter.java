65fc93f62a0dd4c2c98aa7ee253e921545e92cfd
Jeremy Davies <jerdavies@gmail.com>
2011-11-19 21:25:37 +0000

    Implemented the two interface methods. Does nothing at this time

--- reprapgui/src/main/java/com/reprap/reprapgui/printer/PrusaPrinter.java
+++ reprapgui/src/main/java/com/reprap/reprapgui/printer/PrusaPrinter.java
@@ -1,4 +1,17 @@
 package com.reprap.reprapgui.printer;
 
 public class PrusaPrinter implements GenericPrinter {
+
+	@Override
+	public boolean connect() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean disconnect() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
