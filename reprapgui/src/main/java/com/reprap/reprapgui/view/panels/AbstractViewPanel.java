b0c58fdaa42cfbbecb235a2ec54b5d5cbe8ff186
Jeremy Davies <jerdavies@gmail.com>
2011-11-20 20:02:18 +0000

    panel and abstract panel which form a real V in M'V'C

--- reprapgui/src/main/java/com/reprap/reprapgui/view/panels/AbstractViewPanel.java
+++ reprapgui/src/main/java/com/reprap/reprapgui/view/panels/AbstractViewPanel.java
@@ -1,6 +1,7 @@
 package com.reprap.reprapgui.view.panels;
 
 import java.awt.LayoutManager;
+import java.beans.PropertyChangeEvent;
 
 import javax.swing.JPanel;
 
@@ -10,5 +11,13 @@ public abstract class AbstractViewPanel extends JPanel {
 	public AbstractViewPanel(final LayoutManager layout) {
 		super(layout);
 	}
+
+	/**
+     * Called by the controller when it needs to pass along a property change 
+     * from a model.
+     *
+     * @param evt The property change event from the model
+     */
+    public abstract void modelPropertyChange(PropertyChangeEvent evt);
 
 }
--- reprapgui/src/main/java/com/reprap/reprapgui/view/panels/FabricatorPrintPanel.java
+++ reprapgui/src/main/java/com/reprap/reprapgui/view/panels/FabricatorPrintPanel.java
@@ -1,12 +1,44 @@
 package com.reprap.reprapgui.view.panels;
 
-import java.awt.LayoutManager;
+import java.beans.PropertyChangeEvent;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.reprap.reprapgui.controller.utils.StaticConstants;
 
 @SuppressWarnings("serial")
 public class FabricatorPrintPanel extends AbstractViewPanel {
 
-	public FabricatorPrintPanel(final LayoutManager layout) {
-		super(layout);
+	public static final String CONNECTION_LABEL_NAME = "ConnectionLabel";
+
+	@Autowired private JLabel     portLabel, baudSpeedLabel, connectionStateLabel;
+	@Autowired private JTextField portPath, baudSpeed;
+	@Autowired private JButton    printerConnectButton;
+
+	public FabricatorPrintPanel() {
+		super(new MigLayout());
+	}
+	
+	public void initialise() {
+		add(portLabel);
+		add(portPath);
+		add(baudSpeedLabel, "gap unrelated");
+		add(baudSpeed, "wrap");
+		add(printerConnectButton, "span,grow");
+		add(connectionStateLabel, "span,grow");
+
 	}
 
+	@Override
+	public void modelPropertyChange(final PropertyChangeEvent evt) {
+		if(evt.getPropertyName().equals(StaticConstants.CONNECT_STATE)) {
+			connectionStateLabel.setText("Connected");
+		}
+	}
 }
