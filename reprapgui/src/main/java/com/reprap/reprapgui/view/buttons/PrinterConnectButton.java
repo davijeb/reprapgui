7218a8c1a6275e41f9982b902d5e079f278c0ca2
Jeremy Davies <jerdavies@gmail.com>
2011-11-20 23:14:06 +0000

    Now able to switch between connect and disconnect based on the printer button state label

--- reprapgui/src/main/java/com/reprap/reprapgui/view/buttons/PrinterConnectButton.java
+++ reprapgui/src/main/java/com/reprap/reprapgui/view/buttons/PrinterConnectButton.java
@@ -1,14 +1,33 @@
 package com.reprap.reprapgui.view.buttons;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.reprap.reprapgui.controller.PrusaPrinterController;
+import com.reprap.reprapgui.controller.utils.StaticConstants;
+
 @SuppressWarnings("serial")
 public class PrinterConnectButton extends JButton {
 
-	public PrinterConnectButton(final String name, final ActionListener listener) {
+	@Autowired
+	private PrusaPrinterController printController;
+	
+	public PrinterConnectButton(final String name) {
 		super(name);
-		addActionListener(listener);
+		addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(final ActionEvent arg0) {
+				if(getText().equals(StaticConstants.CONNECT_BUTTON)) {
+					printController.connect();
+				} else {
+					printController.disconnect();
+				}
+			}
+		});
 	}
 }
