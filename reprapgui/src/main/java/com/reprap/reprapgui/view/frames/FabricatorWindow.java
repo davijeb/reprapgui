207d789b7d45f31e0039a0d6f53ace8db248ba52
Jeremy Davies <jerdavies@gmail.com>
2011-11-20 20:03:10 +0000

    Added main method using SwingUtilities launch onto the EDT

--- reprapgui/src/main/java/com/reprap/reprapgui/view/frames/FabricatorWindow.java
+++ reprapgui/src/main/java/com/reprap/reprapgui/view/frames/FabricatorWindow.java
@@ -1,90 +1,46 @@
 package com.reprap.reprapgui.view.frames;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-
-import net.miginfocom.swing.MigLayout;
+import javax.swing.SwingUtilities;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.reprap.reprapgui.controller.GenericPrinter;
-import com.reprap.reprapgui.view.components.BasicJTextField;
-import com.reprap.reprapgui.view.config.PrinterButton;
 import com.reprap.reprapgui.view.panels.FabricatorPrintPanel;
 
 /**
  * The RepRapWindow is the main User Interface for controlling the printer,
  * defining the parameters, starting builds etc.
  */
+@SuppressWarnings("serial")
 public class FabricatorWindow extends AbstractFabricatorWindow {
 
-	private static final long serialVersionUID = -7059521570271326438L;
-
 	public static final String MAIN_WINDOW_NAME  = "Main";
-	public static final String CONNECTION_LABEL_NAME  = "ConnectionLabel";
-	public static final String CONNECTION_BUTTON_NAME = "ConnectionButton";
 	
 	@Autowired
-	private GenericPrinter printer;
-
-	private final JLabel portLabel = new JLabel("Port");
-	private final JLabel baudSpeedLabel = new JLabel("Baudspeed");
-	
-	private final JTextField portPath1   = new JTextField(15);
-	private final JTextField baudSpeed   = new JTextField(15);
-	
-	@Autowired
-	private BasicJTextField portPath;
-	
-	private final JButton button = new JButton(PrinterButton.Connect.toString());
-	
-	public FabricatorWindow(final String name) throws Exception {
+	public FabricatorWindow(final String name, final FabricatorPrintPanel panel) throws Exception {
 		super(name);
 		
-		setName(name);
-
-		portPath1.setName("portentry");
-		baudSpeed.setName("baudspeed");
-		button.setName(CONNECTION_BUTTON_NAME);
-
-		final JPanel panel = new FabricatorPrintPanel(new MigLayout());
-		
-		panel.add(portLabel);
-	    panel.add(portPath1);
-	    panel.add(baudSpeedLabel, "gap unrelated");
-	    panel.add(baudSpeed, "wrap");
-	    panel.add(button,"span, grow");
-
 		add(panel);
-		
-		button.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(final ActionEvent arg0) {
-				if(validated())
-				printer.connect();
-			}
-		});
-		
-		pack();
+
+		setName(name);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
-	}
-
-	protected boolean validated() {
-		if(portPath1.getText().equals("") || baudSpeed.getText().equals("")) {
-			final JOptionPane pane = new JOptionPane();
-			pane.showMessageDialog(this, "Connection settings incomplete", "Connection problem", JOptionPane.WARNING_MESSAGE);
-			return false;
-		}
-		return true;
 		
+		pack();
+	}
+	
+	/**
+	 * Spring is used to control the mechanism by which this class is created.
+	 * @param args <not used>
+	 */
+	public static void main(final String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				new ClassPathXmlApplicationContext("application-context.xml").getBean(FabricatorWindow.class);
+			}
+		});
 	}
 }
