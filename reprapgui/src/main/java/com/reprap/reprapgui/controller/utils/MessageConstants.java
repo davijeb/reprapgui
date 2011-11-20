53deb25486b8ed96daf7e5428d4805512a1e8492
Jeremy Davies <jerdavies@gmail.com>
2011-11-20 23:11:36 +0000

    New static constant class

---
+++ reprapgui/src/main/java/com/reprap/reprapgui/controller/utils/MessageConstants.java
@@ -0,0 +1,13 @@
+package com.reprap.reprapgui.controller.utils;
+
+public class MessageConstants {
+	
+	/**
+	 * Connect state constants
+	 */
+	public static final String STATE_CONNECTED        = "Connected";
+	public static final String STATE_DISCONNECTED 	  = "Disconnected";
+	public static final String STATE_CONNECTION_ERROR = "Connection error";
+	
+
+}
---
+++ reprapgui/src/main/java/com/reprap/reprapgui/controller/utils/StaticConstants.java
@@ -0,0 +1,33 @@
+package com.reprap.reprapgui.controller.utils;
+
+public class StaticConstants {
+	
+	/**
+	 * Print [model] static constants
+	 */
+	public static final String PORT	        		 = "Port";
+	public static final String BAUD_SPEED   		 = "BaudSpeed";
+	public static final String ALLOW_CONNECT	     = "ConnectState";
+	
+	/**
+	 * Connect GUI labels
+	 */
+	public static final String PORT_LABEL_NAME	     = "portLabel";
+	public static final String PORT_LABEL_TEXT	     = "Port:";
+	public static final String BAUD_SPEED_LABEL_NAME = "baudSpeedLabel";
+	public static final String BAUD_SPEED_LABEL_TEXT = "Baud speed:";
+	public static final String CONNECT_STATE_LABEL   = "connectStateLabel";
+	
+	/**
+	 * Connect GUI text fields
+	 */
+	public static final String PORT_TEXT_FIELD       = PORT;
+	public static final String BAUD_SPEED_TEXT_FIELD = BAUD_SPEED;
+	
+	/**
+	 * Connect GUI button
+	 */
+	public static final String CONNECT_BUTTON      	 = "Connect";
+	public static final String DISCONNECT_BUTTON     = "Disconnect";
+
+}
