1bb7346f59ada5a9019888f99d48ee6a75ecfbf2
Jeremy Davies <jerdavies@gmail.com>
2011-11-26 14:09:27 +0000

    Now autowire the models and views correctly

--- reprapgui/src/main/java/com/reprap/reprapgui/controller/ApplicationControllerConfiguration.java
+++ reprapgui/src/main/java/com/reprap/reprapgui/controller/ApplicationControllerConfiguration.java
@@ -1,11 +1,36 @@
 package com.reprap.reprapgui.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+
+import com.reprap.reprapgui.model.AbstractModel;
+import com.reprap.reprapgui.view.panels.AbstractViewPanel;
 
 @Configuration
-@Import(ApplicationViewConfiguration.class)
 public class ApplicationControllerConfiguration {
 
-	
+	@Autowired
+	AbstractViewPanel printView, axisView;
+	@Autowired
+	AbstractModel printModel, axisModel;
+
+	@Bean
+	@Autowired
+	public PrinterController printController(final AbstractViewPanel printView,final AbstractModel printModel) {
+		final PrinterController printerController = new PrinterController();
+		printerController.setModel(printModel);
+		printerController.setView(printView);
+		return printerController;
+	}
+
+	@Bean
+	@Autowired
+	public AxisController axisController(final AbstractViewPanel axisView,final AbstractModel axisModel) {
+		final AxisController axisController = new AxisController();
+		axisController.setModel(axisModel);
+		axisController.setView(axisView);
+
+		return axisController;
+	}
 }
